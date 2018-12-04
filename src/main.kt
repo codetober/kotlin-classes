cba1cbb2bb8153d5a7600bf27297fcb802cfffab
Douglas <doug@codetober.com>
2018-12-04 09:53:41 -0600

    removed extra logic
    commented out logic from previous tutorial, only keeping it around for reference.

--- src/main.kt
+++ src/main.kt
@@ -23,23 +23,23 @@ fun main(args: Array<String>) {
     store.printExitMessage()
 }
 
-private fun calcaluateDrinkPrice(drinkName: String, qty: Int): Double {
-    val drinkPrice = when(drinkName){
-        "DarkSide Frap" -> 5.99
-        "Zero-Gravity Mocha" -> 6.99
-        else -> 0.00
-    }
-
-    val drinkTotal = (drinkPrice * qty)
-    return drinkTotal
-}
-
-private fun placeOrder(drinkName: String, customerName: String, qty: Int){
-    val drinkTotal = calcaluateDrinkPrice(drinkName, qty)
-    println("$customerName orders $drinkName(x$qty) for a total of \$$drinkTotal")
-
-}
-
-private fun printWelcomeMessage() = println("Welcome to $STORE_NAME! (No Pictures)\n")
-
-private fun printExitMessage() = println("\nThanks for shopping at $STORE_NAME! (dont' tell your friends)")
+//private fun calcaluateDrinkPrice(drinkName: String, qty: Int): Double {
+//    val drinkPrice = when(drinkName){
+//        "DarkSide Frap" -> 5.99
+//        "Zero-Gravity Mocha" -> 6.99
+//        else -> 0.00
+//    }
+//
+//    val drinkTotal = (drinkPrice * qty)
+//    return drinkTotal
+//}
+//
+//private fun placeOrder(drinkName: String, customerName: String, qty: Int){
+//    val drinkTotal = calcaluateDrinkPrice(drinkName, qty)
+//    println("$customerName orders $drinkName(x$qty) for a total of \$$drinkTotal")
+//
+//}
+//
+//private fun printWelcomeMessage() = println("Welcome to $STORE_NAME! (No Pictures)\n")
+//
+//private fun printExitMessage() = println("\nThanks for shopping at $STORE_NAME! (dont' tell your friends)")
